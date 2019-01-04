9255ee4819085d7ef48627200931502798a348b2
Scott Edwards <scotteza@gmail.com>
2019-01-04 11:55:25 +0000

    Add auto-generated database migration code

--- OdeToFood/Models/OdeToFoodDb.cs
+++ OdeToFood/Models/OdeToFoodDb.cs
@@ -4,6 +4,11 @@ namespace OdeToFood.Models
 {
     public class OdeToFoodDb : DbContext
     {
+        public OdeToFoodDb()
+        {
+
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<RestaurantReview> Reviews { get; set; }
     }
