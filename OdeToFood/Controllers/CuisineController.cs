62b66a0f10fd7cfe61b634d37377fe8e5ecda610
Scott Edwards <scotteza@gmail.com>
2019-01-04 09:48:25 +0000

    Play with custom filters

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Filters;
 
 namespace OdeToFood.Controllers
 {
@@ -11,9 +12,17 @@ namespace OdeToFood.Controllers
         //
         // GET: /Cuisine/
 
+        //[Authorize]
+        [Log]
         public ActionResult Search(string name = "Default Value")
         {
             var message = Server.HtmlEncode(name);
+
+            // throw new Exception("Oh no!!");
+
+            //return File(Server.MapPath(@"~/Content/site.css"), @"text/css");
+
+            //return Json(new { Message = message, Name = "Mr Smith" }, JsonRequestBehavior.AllowGet);
 
             return Content(message);
         }
---
+++ OdeToFood/Filters/LogAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+using ActionFilterAttribute = System.Web.Mvc.ActionFilterAttribute;
+
+namespace OdeToFood.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        // Before execution
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+
+        // After execution
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+
+        // Before result
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+
+        // After result
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
